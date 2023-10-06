55acb541c1551c9a1fea40c9236bebaf3a92978e
Keago99 <keatabde@gmail.com>
2023-10-06 17:57:57 +0200

    re-designed everything, on the right track.

--- scripts.js
+++ scripts.js
@@ -1,18 +1,32 @@
 
-// Player factory for creating new players
-const Player = (name) => {
-    let score = 0;
-    const getName = ()  => name;
-    const getScore = () => score;
-    return{getName, getScore};
+// Player factory to create player objects. 
+const playerFactory = (name, symbol) => {
+    return {name, symbol};
 };
 
-// module for gameboard
-var gameBoard = (function(){
+// BoardModule for things that have to do with the gameboard only
+const boardModule = (() => {
 
+    // An array where the board information is stored, with 9 slots.
+    boardArray = [8];
+
+    // Adding all the ticBoxes to a DOM collection of elements.
+    const ticBoxes = document.querySelectorAll('.ticBox');
+
+    // A function that happens when the user clicks on any of the boards.
+    function clickBox(){
+        this.innerText = "testo";
+    }
+  
+
+    for (i = 0; i < ticBoxes.length; i++){
+        ticBoxes[i].addEventListener('click', clickBox);
+        ticBoxes[i].dataset.Name = i;
+    }
 })();
 
-// module for gamecontroller
-var gameController = (function (){
+
+// a module controller for the game
+const gameController =  (() => {
 
 })();
